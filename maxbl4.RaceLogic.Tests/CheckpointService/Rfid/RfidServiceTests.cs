235eceea3d416a523932e0728e19debb02e4fe1b
Max <maxbl4@gmail.com>
2019-11-15 12:27:25 +0300

    Fix tests

--- maxbl4.RaceLogic.Tests/CheckpointService/Rfid/RfidServiceTests.cs
+++ maxbl4.RaceLogic.Tests/CheckpointService/Rfid/RfidServiceTests.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Easy.MessageHub;
+using maxbl4.RaceLogic.Checkpoints;
 using maxbl4.RaceLogic.Tests.CheckpointService.RfidSimulator;
 using maxbl4.RfidCheckpointService.Services;
 using maxbl4.RfidDotNet;
 using maxbl4.RfidDotNet.AlienTech.ReaderSimulator;
+using maxbl4.RfidDotNet.Infrastructure;
 using Microsoft.Extensions.Logging.Abstractions;
 using Shouldly;
 using Xunit;
@@ -18,12 +21,10 @@ namespace maxbl4.RaceLogic.Tests.CheckpointService.Rfid
     public class RfidServiceTests : IntegrationTestBase
     {
         private readonly FakeSystemClock systemClock;
-        private readonly TagListHandler tagListHandler;
 
         public RfidServiceTests(ITestOutputHelper outputHelper) : base(outputHelper)
         {
             systemClock = new FakeSystemClock();
-            tagListHandler = WithStorageService(storageService => new SimulatorBuilder(storageService).Build());
         }
 
         [Fact]
@@ -31,6 +32,7 @@ namespace maxbl4.RaceLogic.Tests.CheckpointService.Rfid
         {
             WithStorageService(storageService =>
             {
+                var tagListHandler = new SimulatorBuilder(storageService).Build();
                 var rfidService = new RfidService(storageService, new MessageHub(), systemClock,
                     new NullLogger<RfidService>());
                 var connectionString = storageService.GetRfidOptions().GetConnectionString();
@@ -47,7 +49,23 @@ namespace maxbl4.RaceLogic.Tests.CheckpointService.Rfid
         [Fact]
         public void Should_store_checkpoint_before_emitting_to_observable()
         {
-            throw new NotImplementedException();
+            WithStorageService(storageService =>
+            {
+                var tagListHandler = new SimulatorBuilder(storageService).Build();
+                List<Checkpoint> cps = null;
+                var messageHub = new MessageHub();
+                messageHub.Subscribe<Checkpoint>(x => cps = storageService.ListCheckpoints());
+                var rfidService = new RfidService(storageService, messageHub, systemClock,
+                    new NullLogger<RfidService>());
+                
+                
+                tagListHandler.ReturnOnce(new Tag {TagId = "1"}).Wait(1000);
+                
+                
+                Timing.StartWait(() => cps != null).Result.ShouldBeTrue();
+                cps.Count.ShouldBe(1);
+                cps[0].RiderId.ShouldBe("1");
+            });
         }
     }
 }
