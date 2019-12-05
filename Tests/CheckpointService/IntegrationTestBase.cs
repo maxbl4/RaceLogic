f64c579513c37f3908fdbfc96e6f8d42e204f9f6
Max <maxbl4@gmail.com>
2019-12-05 22:06:31 +0000

    Refactor naming scheme

---
+++ Tests/CheckpointService/IntegrationTestBase.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using AutoMapper;
+using Easy.MessageHub;
+using maxbl4.Infrastructure;
+using maxbl4.Infrastructure.Extensions.TestOutputHelperExt;
+using maxbl4.Race.CheckpointService;
+using maxbl4.Race.CheckpointService.Services;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Serilog;
+using Xunit.Abstractions;
+
+namespace maxbl4.Race.Tests.CheckpointService
+{
+    public class IntegrationTestBase
+    {
+        static object sync = new object();
+        private readonly ThreadLocal<IMessageHub> messageHub = new ThreadLocal<IMessageHub>(() => new MessageHub());
+        protected IMessageHub MessageHub => messageHub.Value;
+        protected readonly string storageConnectionString;
+        protected readonly FakeSystemClock SystemClock = new FakeSystemClock();
+        protected ILogger Logger { get; }
+        protected IMapper Mapper { get; }
+        
+        public IntegrationTestBase(ITestOutputHelper outputHelper)
+        {
+            lock (sync)
+            {
+                CheckpointServiceRunner.SetupLogger("testsettings");
+            }
+            Logger = Log.ForContext(this.GetType());
+            
+            var fileName = GetNameForDbFile(outputHelper);
+            Logger.Debug("Storage {@fileName}", fileName);
+            new RollingFileInfo(fileName).Delete();
+            storageConnectionString = $"Filename={fileName};UtcDate=true";
+            Mapper = new MapperConfiguration(x => x.AddMaps(typeof(Startup)))
+                .CreateMapper();
+        }
+
+        public void WithStorageService(Action<StorageService> storageServiceInitializer)
+        {
+            Logger.Debug("Creating StorageServiceService with {@storageConnectionString}", storageConnectionString);
+            using var storageService = new StorageService(Options.Create(new ServiceOptions{StorageConnectionString = storageConnectionString}), 
+                MessageHub, SystemClock);
+            storageServiceInitializer(storageService);
+        }
+        
+        public void WithRfidService(Action<StorageService, RfidService> action)
+        {
+            Logger.Debug("Creating StorageServiceService with {@storageConnectionString}", storageConnectionString);
+            using var storageService = new StorageService(Options.Create(new ServiceOptions{StorageConnectionString = storageConnectionString}),
+                MessageHub, SystemClock);
+            using var rfidService = new RfidService(storageService, MessageHub, SystemClock, Mapper);
+            action(storageService, rfidService);
+        }
+        
+        public T WithStorageService<T>(Func<StorageService, T> storageServiceInitializer)
+        {
+            Logger.Debug("Creating StorageServiceService with {@storageConnectionString}", storageConnectionString);
+            using var storageService = new StorageService(Options.Create(new ServiceOptions{StorageConnectionString = storageConnectionString}),
+                MessageHub, SystemClock);
+            return storageServiceInitializer(storageService);
+        }
+        
+        public CheckpointServiceRunner CreateCheckpointService(int pauseStartupMs = 0)
+        {
+            Logger.Debug("Creating CheckpointService with {@storageConnectionString}", storageConnectionString);
+            var svc = new CheckpointServiceRunner();
+            svc.Start(new []
+            {
+                $"--ServiceOptions:StorageConnectionString={storageConnectionString}", 
+                $"--ServiceOptions:PauseInStartupMs={pauseStartupMs}",
+                $"--Environment={Environments.Development}"
+            }).Wait(0);
+            return svc;
+        }
+
+        string GetNameForDbFile(ITestOutputHelper outputHelper)
+        {
+            Directory.CreateDirectory("var/data");
+            var parts = outputHelper.GetTest().DisplayName.Split(".");
+            return "var/data/" + "_" + string.Join("-", parts.Skip(Math.Max(parts.Length - 2, 0))) + ".litedb";
+        }
+    }
+}
---
+++ Tests/FakeSystemClock.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reactive.PlatformServices;
+
+namespace maxbl4.Race.Tests
+{
+    public class FakeSystemClock : ISystemClock
+    {
+        private bool useRealClock = false;
+        private DateTime now = new DateTime(2019, 1,1, 0, 0, 0, DateTimeKind.Utc);
+        public DateTimeOffset UtcNow => Now;
+
+        public DateTime Now
+        {
+            get
+            {
+                if (useRealClock)
+                    return DateTime.UtcNow;
+                return now;
+            }
+            set
+            {
+                useRealClock = false;
+                now = value;
+            }
+        }
+
+        public void UseRealClock()
+        {
+            useRealClock = true;
+        }
+
+        public DateTime Advance(TimeSpan? by = null)
+        {
+            if (by == null) by = TimeSpan.FromSeconds(1);
+            Now = Now.Add(by.Value);
+            return Now;
+        }
+    }
+}
