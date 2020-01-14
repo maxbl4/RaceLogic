d3903063159dc16122454873ac75f92338e74cde
Max <maxbl4@gmail.com>
2020-01-14 18:44:04 +0300

    Basic implementation of Session Log

---
+++ Logic/LogManagement/EntryTypes/InsertCheckpoint.cs
@@ -0,0 +1,9 @@
+﻿using maxbl4.Race.Logic.Checkpoints;
+
+namespace maxbl4.Race.Logic.LogManagement.EntryTypes
+{
+    public class InsertCheckpoint : Checkpoint
+    {
+        
+    }
+}
--- Logic/LogManagement/SessionLog.cs
+++ Logic/LogManagement/SessionLog.cs
@@ -13,20 +13,34 @@ namespace maxbl4.Race.Logic.LogManagement
         public ITrackOfCheckpoints TrackOfCheckpoints { get; private set; } = new TrackOfCheckpoints();
         public IFinishCriteria FinishCriteria { get; private set; } = null;
         public DateTime StartTime = Constants.DefaultUtcDate;
+
+        public void BatchLoad(IEnumerable<IEntry> logEntries)
+        {
+            LogEntries.Clear();
+            LogEntries.AddRange(logEntries);
+            var start = LogEntries.OfType<SessionStart>().LastOrDefault();
+            if (start != null)
+                ApplyStart(start);
+            else
+                TrackOfCheckpoints = ReloadTrack(StartTime, FinishCriteria);
+        }
         
         public void Start(SessionStart start)
         {
             LogEntries.Add(start);
-            StartTime = start.Timestamp;
-            FinishCriteria = new FinishCriteria(start.Duration, start.TotalLaps, start.LapsAfterDuration,
-                start.SkipStartingCheckpoint, start.ForceFinishOnly);
-            TrackOfCheckpoints = ReloadTrack(StartTime, FinishCriteria);
+            ApplyStart(start);
         }
 
         public void Checkpoint(Checkpoint checkpoint)
         {
             LogEntries.Add(checkpoint);
             TrackOfCheckpoints.Append(checkpoint);
+        }
+        
+        public void InsertCheckpoint(InsertCheckpoint insert)
+        {
+            LogEntries.Add(insert);
+            TrackOfCheckpoints = ReloadTrack(StartTime, FinishCriteria);
         }
         
         public void DropCheckpoint(DropCheckpoint drop)
@@ -39,23 +53,45 @@ namespace maxbl4.Race.Logic.LogManagement
         {
             LogEntries.Add(comment);
         }
+        
+        void ApplyStart(SessionStart start)
+        {
+            StartTime = start.Timestamp;
+            FinishCriteria = new FinishCriteria(start.Duration, start.TotalLaps, start.LapsAfterDuration,
+                start.SkipStartingCheckpoint, start.ForceFinishOnly);
+            TrackOfCheckpoints = ReloadTrack(StartTime, FinishCriteria);
+        }
 
         ITrackOfCheckpoints ReloadTrack(DateTime startTime, IFinishCriteria finishCriteria)
         {
             var track = new TrackOfCheckpoints(startTime, finishCriteria);
-            foreach (var checkpoint in FlattenCheckpointLog())
+            foreach (var checkpoint in FlattenCheckpointLog(LogEntries))
             {
                 track.Append(checkpoint);
             }
             return track;
         }
 
-        IEnumerable<Checkpoint> FlattenCheckpointLog()
+        public static IEnumerable<Checkpoint> FlattenCheckpointLog(IEnumerable<IEntry> logEntries)
         {
-            var drops = new HashSet<long>(LogEntries.OfType<DropCheckpoint>().Select(x => x.Id));
-            //TODO: Apply dropped/altered checkpoints before returning
-            return LogEntries.OfType<Checkpoint>()
-                .Where(x => !drops.Contains(x.Id));
+            var drops = new HashSet<long>(logEntries.OfType<DropCheckpoint>().Select(x => x.Id));
+            var inserts = logEntries.OfType<InsertCheckpoint>().Where(x => !drops.Contains(x.Id))
+                .OrderBy(x => x.Timestamp)
+                .ToList();
+            foreach (var checkpoint in logEntries.OfType<Checkpoint>().Where(x => !drops.Contains(x.Id)))
+            {
+                while (inserts.Count > 0 && inserts[0].Timestamp < checkpoint.Timestamp)
+                {
+                    yield return inserts[0];
+                    inserts.RemoveAt(0);
+                }
+                yield return checkpoint;
+            }
+
+            foreach (var insert in inserts)
+            {
+                yield return insert;
+            }
         }
     }
 }
