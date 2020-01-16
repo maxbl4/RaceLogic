1e29a4ce5f3b127cbe328a195db56bab525aaa7b
Max <maxbl4@gmail.com>
2020-01-16 20:52:38 +0300

    RecordingSession WIP

--- Logic/EventModel/Storage/Model/CheckpointDto.cs
+++ Logic/EventModel/Storage/Model/CheckpointDto.cs
@@ -1,24 +1,14 @@
-﻿using System;
-using maxbl4.Race.Logic.Checkpoints;
-using maxbl4.Race.Logic.EventModel.Storage.Identifier;
+﻿using maxbl4.Race.Logic.EventModel.Storage.Identifier;
 using maxbl4.Race.Logic.EventStorage.Storage.Traits;
-using maxbl4.Race.Logic.LogManagement.EntryTypes;
 
 namespace maxbl4.Race.Logic.EventStorage.Storage.Model
 {
-    public class CheckpointDto: IHasId<CheckpointDto>, IHasSeed, ICheckpoint
+    public class CheckpointDto: IHasId<CheckpointDto>, IHasSeed
     {
-        private long id;
-        public DateTime Timestamp { get; set; }
-
-        long IEntry.Id
-        {
-            get => id;
-            set => id = value;
-        }
-
         public Id<CheckpointDto> Id { get; set; }
         public bool IsSeed { get; set; }
+        
+        public Id<RecordingSessionDto> RecordingSessionId { get; set; }
         public string RiderId { get; set; }
     }
 }
---
+++ Logic/EventModel/Storage/Model/RecordingSessionDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using maxbl4.Race.Logic.EventModel.Storage.Identifier;
+using maxbl4.Race.Logic.EventStorage.Storage.Traits;
+
+namespace maxbl4.Race.Logic.EventStorage.Storage.Model
+{
+    public class RecordingSessionDto: IHasId<RecordingSessionDto>, IHasSeed, IHasTimestamp, IHasName
+    {
+        public Id<RecordingSessionDto> Id { get; set; }
+        public bool IsSeed { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
